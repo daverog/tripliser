265eabaa47f7d005a9d1d4522990b086fc9a7dc9
David Rogers <rogerd01@MC-S056121.local>
2011-05-05 12:36:14 +0100

    Added script, updated docs

--- src/test/java/org/daverog/tripliser/mapping/model/PropertyMappingTest.java
+++ src/test/java/org/daverog/tripliser/mapping/model/PropertyMappingTest.java
@@ -1,8 +1,16 @@
 package org.daverog.tripliser.mapping.model;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 
 public class PropertyMappingTest {
 
 	//TODO test validate method
+	@Test
+	@Ignore
+	public void test(){
+
+	}
 
 }
